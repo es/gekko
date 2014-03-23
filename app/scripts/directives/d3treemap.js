27e10c78302d2f5ef887ae64ec07fa534c103b4a
Emil Stolarsky <emil.stolarsky@gmail.com>
2014-03-23 06:51:32 -0400

    Changed treemap nodes to be svg objs and improve text style

--- app/scripts/directives/d3treemap.js
+++ app/scripts/directives/d3treemap.js
@@ -20,21 +20,44 @@ angular.module('geckoApp')
                                  .sticky(true)
                                  .value(function(d) { return d.weight; });
   
-          var div = d3.select(element[0]).append('div')
-              .style('position', 'relative')
-              .style('width', (width + margin.left + margin.right) + 'px')
-              .style('height', (height + margin.top + margin.bottom) + 'px')
-              .style('margin', '0 auto');
+          var svg = d3.select(element[0]).append('div')
+                      .style('position', 'relative')
+                      .style('width', (width + margin.left + margin.right) + 'px')
+                      .style('height', (height + margin.top + margin.bottom) + 'px')
+                      .style('margin', '0 auto')
+                      .append("svg:svg")
+                      .attr("width", width)
+                      .attr("height", height)
+                      .append("svg:g")
+                      .attr("transform", "translate(.5,.5)");
           
           TreemapDataService.onload(function (err, data) {
             if (err) console.error(err);
-            var node = div.datum(data).selectAll('.node')
-                .data(treemap.nodes)
-                .enter().append('div')
-                .attr('class', 'node')
+            var root, node;
+            node = root = data;
+            var nodes = treemap.nodes(root)
+                               .filter(function(d) { return !d.children; });
+
+            var cell = svg.datum(data).selectAll(".cell")
+                          .data(nodes)
+                          .enter().append("svg:g")
+                          .attr("class", "cell")
+                          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+                          /*.on("click", function(d) { return zoom(node == d.parent ? root : d.parent); })*/;
+
+            cell.append("svg:rect")
                 .call(position)
-                .style('background', function(d) { return color(d.name); })
-                .text(function(d) { return d.name; });
+                .style("fill", function(d) { return color(d.parent.name); });
+
+            cell.append("svg:text")
+                .attr("x", function(d) { return d.dx / 2; })
+                .attr("y", function(d) { return d.dy / 2; })
+                .attr("dy", ".35em")
+                .attr("class", "font-smoothing")
+                .attr("text-anchor", "middle")
+                .text(function(d) { return d.name; })
+                .style("writing-mode", function (d) { d.w = this.getComputedTextLength(); return d.dx < d.w && d.dy > d.w ? 'tb':'';})
+                .style("opacity", function(d) { return d.dx > d.w || (d.dy > d.w) ? 1 : 0; });
             
             scope.$watch(function () { 
                 return scope.$parent.dateSelected;
@@ -43,14 +66,24 @@ angular.module('geckoApp')
                 if ( newVal !== oldVal ) {
                   var newDate = angular.isDate(newVal)? newVal.toISOString().substring(0, 10) 
                                                       : newVal;
-                  // console.log('newDate:', newDate);
                   TreemapDataService.setDate(newDate, function (err, data) {
                     if (err) return console.error(err);
-                    node/*.datum(data)*/
-                        .data(treemap.nodes)
+                    treemap.nodes(newDate)
+                               .filter(function(d) { return !d.children; });
+
+                    var t = svg.selectAll("g.cell")
                         .transition()
                         .duration(1500)
-                        .call(position);
+                        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+                    t.select("rect")
+                        .attr("width", function(d) { return d.dx - 1; })
+                        .attr("height", function(d) { return  d.dy - 1; })
+
+                    t.select("text")
+                        .attr("x", function(d) { return d.dx / 2; })
+                        .attr("y", function(d) { return d.dy / 2; })
+                        .style("opacity", function(d) { return d.dx > d.w || (d.dy > d.w) ? 1 : 0; });
                   });
                 }
               }
@@ -60,8 +93,8 @@ angular.module('geckoApp')
           function position() {
             this.style('left', function(d) { return d.x + 'px'; })
                 .style('top', function(d) { return d.y + 'px'; })
-                .style('width', function(d) { return Math.max(0, d.dx - 1) + 'px'; })
-                .style('height', function(d) { return Math.max(0, d.dy - 1) + 'px'; });
+                .attr('width', function(d) { return Math.max(0, d.dx - 1) + 'px'; })
+                .attr('height', function(d) { return Math.max(0, d.dy - 1) + 'px'; });
           }
         }
       };
